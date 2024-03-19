3cc7aa7314c7e917eb3c39210331241cffaf72d2
InnaMnatsakanyan <inna.mnacakanyan1@gmail.com>
2024-03-19 19:28:48 +0400

    Weather view and view model

---
+++ src/presentation/screen/weather/WeatherView.tsx
@@ -0,0 +1,12 @@
+import WeatherViewModel from "./WeatherViewModel"
+
+export default function WeatherView() {
+    const {state, onSearchTextChange, onSearchClick} = WeatherViewModel()
+    return (
+        <div>
+            <input type="text" value={state.city} onChange={onSearchTextChange} />
+            <button onClick={onSearchClick}>Search</button>
+            <p>Current value: {state.counter}</p>
+        </div>
+    )
+}
---
+++ src/presentation/screen/weather/WeatherViewModel.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+class WeatherState {
+    readonly city: string = ""
+    readonly counter: number = 0
+}
+
+export default function WeatherViewModel() {
+    const [ state, setState] = useState(new WeatherState())
+
+    function onSearchTextChange(changeEvent: React.ChangeEvent<HTMLInputElement>) {
+        setState((prevState) => ({
+            ...prevState,
+            city: changeEvent.target.value,
+            counter: prevState.counter + 1
+        }))
+    }
+
+    function onSearchClick(_: React.MouseEvent<HTMLElement>) {
+        setState((prevState) => ({
+            ...prevState,
+            counter: prevState.counter + 100
+        }))
+    }
+
+    return {
+        state,
+        onSearchTextChange,
+        onSearchClick
+    }
+}
